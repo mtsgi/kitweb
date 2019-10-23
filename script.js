1094fd2b321255d48667efc39ff9a0b112e456fb
mtsgi <oq@live.jp>
2019-10-24 00:10:10 +0900

    Add Preloader and Copy Style

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ $(() => {
     for( let i of document.querySelectorAll('.emoji') ){
         i.innerHTML = joypixels.toImage( i.innerHTML );
     }
+    $('main').css('opacity', '1.0');
+    $('.preloader-wrapper').fadeOut(500);
 });
